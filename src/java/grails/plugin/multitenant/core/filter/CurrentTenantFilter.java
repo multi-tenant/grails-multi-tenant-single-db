b70ca15609bf3e3c0580860658a22deab52385e0
Kim A. Betti <kim.betti@gmail.com>
2011-01-10 21:06:25 +0100

    Make sure that current-tenant thread local storage is reset in the event of an exception

--- src/java/grails/plugin/multitenant/core/filter/CurrentTenantFilter.java
+++ src/java/grails/plugin/multitenant/core/filter/CurrentTenantFilter.java
@@ -11,10 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+/**
+ * 
+ * @author Kim A. Betti
+ */
 public class CurrentTenantFilter implements Filter {
 
-    private CurrentTenant currentTenant;
-    private TenantResolver tenantResolver;
+    private CurrentTenant currentTenant; // thread local storage
+    private TenantResolver tenantResolver; // provided by implementation 
 
     @Override
     public void init(FilterConfig fc) throws ServletException {
@@ -23,23 +27,26 @@ public class CurrentTenantFilter implements Filter {
         currentTenant = (CurrentTenant) ctx.getBean("currentTenant");
         tenantResolver = (TenantResolver) ctx.getBean("tenantResolver");
     }
-
+   
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain filterChain) throws IOException, ServletException {
-
-        if (request instanceof HttpServletRequest) {
-            HttpServletRequest httpRequest = (HttpServletRequest) request;
-            int currentTenantId = tenantResolver.resolve(httpRequest);
-            currentTenant.set(currentTenantId);
-        }
-
-        filterChain.doFilter(request, response);
-        currentTenant.set(-1);
+    		FilterChain filterChain) throws IOException, ServletException {
+    	
+    	try {
+	        if (request instanceof HttpServletRequest) {
+	            HttpServletRequest httpRequest = (HttpServletRequest) request;
+	            int currentTenantId = tenantResolver.resolve(httpRequest);
+	            currentTenant.set(currentTenantId);
+	        }
+	
+	        filterChain.doFilter(request, response);
+    	} finally {
+    		currentTenant.set(-1);
+    	}
     }
 
     @Override
     public void destroy() {
-    }	
+    }
 	
 }
