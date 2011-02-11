5e37b074103b67d23b2eda2054501d32b882a826
Kim A. Betti <kim.betti@gmail.com>
2011-02-11 12:50:14 +0100

    Fixed formating, indentation with spaces instead of tabs

--- src/java/grails/plugin/multitenant/core/util/TenantUtils.java
+++ src/java/grails/plugin/multitenant/core/util/TenantUtils.java
@@ -12,46 +12,52 @@ import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 
 /**
+ * TODO: This class needs some refactoring. Having all these
+ * different responsibilities in the same class smells a bit. 
  * 
  * @author Kim A. Betti <kim@developer-b.com>
  */
 public class TenantUtils {
-	
-	private static CurrentTenant currentTenant;
-	private static SessionFactory sessionFactory;
-	
-	public static Class<?> TENANT_DOMAIN_CLASS;
-		
-	public static boolean hasMultiTenantAnnotation(DefaultGrailsDomainClass domainClass) {
-		return hasMultiTenantAnnotation(domainClass.getClazz());
-	}
-	
-	public static boolean hasMultiTenantAnnotation(Class<?> clazz) {
-	    Annotation[] annotations = clazz.getAnnotations();
-        for (Annotation annotation : annotations)
-            if (annotation instanceof MultiTenant)
+
+    private static CurrentTenant currentTenant;
+    private static SessionFactory sessionFactory;
+
+    public static Class<?> TENANT_DOMAIN_CLASS;
+
+    public static boolean hasMultiTenantAnnotation(
+            DefaultGrailsDomainClass domainClass) {
+        return hasMultiTenantAnnotation(domainClass.getClazz());
+    }
+
+    public static boolean hasMultiTenantAnnotation(Class<?> clazz) {
+        Annotation[] annotations = clazz.getAnnotations();
+        for (Annotation annotation : annotations) {
+            if (annotation instanceof MultiTenant) {
                 return true;
-        
+            }
+        }
+
         return false;
-	}
-	
-	public static boolean isMultiTenantClass(Class<?> clazz) {
-		if (hasMultiTenantAnnotation(clazz)) {
-			return true;
-		} else {
-			Class<?> superclass = clazz.getSuperclass();
-			if (superclass != null && superclass != Object.class) 
-				return isMultiTenantClass(superclass);
-		}
-		
-		return false;
-	}
-	
-	public void withTenantId(Integer temporaryTenantId, Closure closure) {
-	    Integer previousTenantId = currentTenant.get();
+    }
+
+    public static boolean isMultiTenantClass(Class<?> clazz) {
+        if (hasMultiTenantAnnotation(clazz)) {
+            return true;
+        } else {
+            Class<?> superclass = clazz.getSuperclass();
+            if (superclass != null && superclass != Object.class) {
+                return isMultiTenantClass(superclass);
+            }
+        }
+
+        return false;
+    }
+
+    public void withTenantId(Integer temporaryTenantId, Closure closure) {
+        Integer previousTenantId = currentTenant.get();
         Session currentSession = sessionFactory.getCurrentSession();
         currentSession.flush();
-        
+
         try {
             enableHibernateFilter(currentSession, temporaryTenantId);
             currentTenant.set(temporaryTenantId);
@@ -62,18 +68,18 @@ public class TenantUtils {
             currentTenant.set(previousTenantId);
         }
     }
-	
-	public static void enableHibernateFilter(Session session, Integer tenantId) {
-		session.enableFilter(TenantFilterCfg.TENANT_FILTER_NAME)
-        	.setParameter(TenantFilterCfg.TENANT_ID_PARAM_NAME, tenantId);
-	}
-	
-	public void setCurrentTenant(CurrentTenant currentTenant) {
-		TenantUtils.currentTenant = currentTenant;
-	}
-	
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		TenantUtils.sessionFactory = sessionFactory;
-	}
-	
+
+    public static void enableHibernateFilter(Session session, Integer tenantId) {
+        session.enableFilter(TenantFilterCfg.TENANT_FILTER_NAME).setParameter (
+                TenantFilterCfg.TENANT_ID_PARAM_NAME, tenantId);
+    }
+
+    public void setCurrentTenant(CurrentTenant currentTenant) {
+        TenantUtils.currentTenant = currentTenant;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        TenantUtils.sessionFactory = sessionFactory;
+    }
+
 }
