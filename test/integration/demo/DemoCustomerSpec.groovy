f1ae994aafb34c3ed2e4fc1b8b4281156ab0c226
Joshua Burnett <joshua@greenbill.com>
2012-06-03 01:00:29 -0500

    cleanup and fix tests. merge into all spock tests

--- test/integration/demo/DemoCustomerSpec.groovy
+++ test/integration/demo/DemoCustomerSpec.groovy
@@ -8,21 +8,30 @@ import grails.plugin.spock.IntegrationSpec
  * @author Kim A. Betti
  */
 class DemoCustomerSpec extends IntegrationSpec {
+    //FIXME This used to work? I think this should work without needing to turn transactional off but it does not.
+    //if its transactional then the session in the withThisTenant cannot see the customer
+    static transactional = false
 
     def testTenant
     
     def setup() {
-        testTenant = new DemoTenant(name: "test tenant", domain: "test.com")
+        testTenant = new DemoTenant(name: "DemoCustomerSpec tenant", domain: "DemoCustomerSpec.com")
         testTenant.save(flush: true, failOnError: true)
     }
     
     def "instances are available for tenants"() {
         given: "a customer instance created outside a tenant namespace"
-        new DemoCustomer(name: "Customer A").save(failOnError: true, flush: true)
+        def c = new DemoCustomer(name: "DemoCustomerSpec A").save(failOnError: true, flush: true)
+        assert c
+        println c.id
         
         expect: "we should be able to look up the customer inside a tenant namespace"
+        assert DemoCustomer.findByName("DemoCustomerSpec A")
         testTenant.withThisTenant {
-            DemoCustomer.findByName("Customer A")
+            DemoCustomer.get(1)
+        }
+        testTenant.withThisTenant {
+            DemoCustomer.findByName("DemoCustomerSpec A")
         }
     }
 
