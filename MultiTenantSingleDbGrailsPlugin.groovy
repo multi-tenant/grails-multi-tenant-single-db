22854e63abb81d07737ba3b9ea6b2203219de90b
Stephen Ronderos <steve@ronderos.com>
2013-04-17 22:06:12 -0500

    Resolve issue with exceptions being thrown from getWebXmlFilterOrder Needed to import classes Also did some general code cleanup

--- MultiTenantSingleDbGrailsPlugin.groovy
+++ MultiTenantSingleDbGrailsPlugin.groovy
@@ -1,6 +1,12 @@
 import grails.plugin.multitenant.singledb.MtSingleDbPluginSupport
+import grails.util.Environment
+
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class MultiTenantSingleDbGrailsPlugin {
+	private Logger log = LoggerFactory.getLogger('grails.plugin.multiTenant.MultiTenantSingleDbPlugin')
 
     def version = "0.8.3-SNAPSHOT"
     def grailsVersion = "1.3.5 > *"
@@ -31,16 +37,26 @@ class MultiTenantSingleDbGrailsPlugin {
 	
     // make sure the filter chain filter is after the Grails filter
 	def getWebXmlFilterOrder() {
-        def slurper = new ConfigSlurper(Environment.getCurrent().getName())
-        def config = slurper.parse(getClass().getClassLoader().loadClass(GrailsApplication.CONFIG_CLASS))
-
-        if(config.multiTenant.resolveTenantBeforeLogin) {
-    		def SecurityFilterPosition = getClass().getClassLoader().loadClass('org.codehaus.groovy.grails.plugins.springsecurity.SecurityFilterPosition')
-    		[tenantFilter: SecurityFilterPosition.FORM_LOGIN_FILTER.order - 100]
-        } else {
-    		def FilterManager = getClass().getClassLoader().loadClass('grails.plugin.webxml.FilterManager')
-    		[tenantFilter: FilterManager.SITEMESH_POSITION - 100]
-        }
+		log.debug("Start getWebXmlFilterOrder")
+		def filterMap = [:]
+		try {
+			def classLoader = new GroovyClassLoader(getClass().getClassLoader())
+	        def slurper = new ConfigSlurper(Environment.getCurrent().getName())
+	        def config = slurper.parse(classLoader.loadClass(GrailsApplication.CONFIG_CLASS))
+			
+	        if(config.multiTenant.resolveTenantBeforeLogin) {
+	    		def SecurityFilterPosition = classLoader.loadClass('org.codehaus.groovy.grails.plugins.springsecurity.SecurityFilterPosition')
+				log.debug("set WebXmlFilterOrder before login")
+				filterMap = [tenantFilter: SecurityFilterPosition.FORM_LOGIN_FILTER.order - 100]
+	        } else {
+	    		def FilterManager = classLoader.loadClass('grails.plugin.webxml.FilterManager')
+				log.debug("set WebXmlFilterOrder after login")
+				filterMap = [tenantFilter: FilterManager.SITEMESH_POSITION - 100]
+	        }
+		} catch (ClassNotFoundException e) {
+			log.warn "Could not determine desired tenantFilter position."
+		}
+		return filterMap
 	}
 
     def doWithSpring = MtSingleDbPluginSupport.doWithSpring
