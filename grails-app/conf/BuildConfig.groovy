d5f6d3dfa2681634a828d8c4c140ca7a0c987909
Omasiri <omasiri@novadge.com>
2014-07-20 16:51:59 +0100

    BuildConfig tweaks

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
         }
 
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3'{
 			export = false
 		}
 //                build ":tomcat:7.0.53"
