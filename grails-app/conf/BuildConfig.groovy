1c478f859f3bf2df45fe3962c81613c83a139e66
Stephen Ronderos <steve@ronderos.com>
2014-07-22 21:16:08 -0500

    Remove comments and clean up whitespace

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,42 +21,37 @@ grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
 
 	// inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
+	inherits("global") {
+	}
+	
 	log 'warn'
 
 	repositories {
-            grailsCentral()
-            mavenLocal()
-            mavenCentral()
+			grailsCentral()
+			mavenLocal()
+			mavenCentral()
 	}
-        dependencies {
-            // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-            // runtime 'mysql:mysql-connector-java:5.1.29'
-            // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
-
-            //compile "org.springframework:spring-orm:$springVersion"
-            //test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
-        }
+	
+	dependencies {
+	}
 
 	plugins {
 		build(":release:3.0.1",":rest-client-builder:1.0.3") {
-                    export = false
-                }
-//                build ":tomcat:7.0.53"
+			export = false
+		}
+		
 		provided ':webxml:1.4.1'
 
 		compile(":hibernate:3.6.10.15") { export = false }
-                compile ':cache:1.1.6'
+		
+		compile ':cache:1.1.6'
+		
 		compile(':hawk-eventing:0.5.1') {
 			excludes 'svn'
 		}
 
 		compile(':hibernate-hijacker:0.8.1') {
 			excludes 'svn'
-		}
-                
+		}		
 	}
 }
