5a9141f70f223dda3c32f0ccab26fe58fbadb585
Cleydson Júnior <cleydson.fibonacci@solucoesageis.com.br>
2014-07-23 11:15:22 -0300

    Changing how the plugin check if it should resolve tenant before login

--- MultiTenantSingleDbGrailsPlugin.groovy
+++ MultiTenantSingleDbGrailsPlugin.groovy
@@ -1,19 +1,18 @@
 import grails.plugin.multitenant.singledb.MtSingleDbPluginSupport
-import grails.util.Environment
-import org.codehaus.groovy.grails.commons.GrailsApplication
+import grails.util.Holders
 
 class MultiTenantSingleDbGrailsPlugin {
 
-    def version = "0.8.3"
+    def version = "0.8.4-SNAPSHOT"
     def grailsVersion = "1.3.5 > *"
 
     def loadAfter = [
-        'hawk-eventing',
-        'hibernate-hijacker',
-        'controllers'
+            'hawk-eventing',
+            'hibernate-hijacker',
+            'controllers'
     ]
 
-    def pluginExcludes = ["**/demo/**" ]
+    def pluginExcludes = ["**/demo/**"]
 
     def author = "Kim A. Betti"
     def authorEmail = "kim@developer-b.com"
@@ -24,10 +23,10 @@ class MultiTenantSingleDbGrailsPlugin {
 
     def license = "APACHE"
     def developers = [
-        [ name: "Steve Ronderos", email: "steve.ronderos@gmail.com" ]
+            [name: "Steve Ronderos", email: "steve.ronderos@gmail.com"]
     ]
-    def issueManagement = [ system: "github", url: "https://github.com/multi-tenant/grails-multi-tenant-single-db/issues" ]
-    def scm = [ url: "https://github.com/multi-tenant/grails-multi-tenant-single-db" ]
+    def issueManagement = [system: "github", url: "https://github.com/multi-tenant/grails-multi-tenant-single-db/issues"]
+    def scm = [url: "https://github.com/multi-tenant/grails-multi-tenant-single-db"]
 
     // make sure the filter chain filter is after the Grails filter
     def getWebXmlFilterOrder() {
@@ -35,10 +34,8 @@ class MultiTenantSingleDbGrailsPlugin {
         def filterMap = [:]
         try {
             def classLoader = new GroovyClassLoader(getClass().getClassLoader())
-            def slurper = new ConfigSlurper(Environment.getCurrent().getName())
-            def config = slurper.parse(classLoader.loadClass(GrailsApplication.CONFIG_CLASS))
-
-            if(config.multiTenant.resolveTenantBeforeLogin) {
+            def config = Holders.config
+            if (config.multiTenant.resolveTenantBeforeLogin) {
                 def SecurityFilterPosition = classLoader.loadClass('org.codehaus.groovy.grails.plugins.springsecurity.SecurityFilterPosition')
                 log.debug("set WebXmlFilterOrder before login")
                 filterMap = [tenantFilter: SecurityFilterPosition.FORM_LOGIN_FILTER.order - 100]
@@ -47,20 +44,20 @@ class MultiTenantSingleDbGrailsPlugin {
                 log.debug("set WebXmlFilterOrder after login")
                 filterMap = [tenantFilter: FilterManager.SITEMESH_POSITION - 100]
             }
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException ignored) {
             log.warn "Could not determine desired tenantFilter position."
         }
         return filterMap
     }
 
     def doWithSpring = {
-		 MtSingleDbPluginSupport.doWithSpring.delegate = delegate
-		 MtSingleDbPluginSupport.doWithSpring application
+        MtSingleDbPluginSupport.doWithSpring.delegate = delegate
+        MtSingleDbPluginSupport.doWithSpring application
     }
 
     def doWithDynamicMethods = { ctx ->
-		 MtSingleDbPluginSupport.doWithDynamicMethods.delegate = delegate
-		 MtSingleDbPluginSupport.doWithDynamicMethods ctx, application
+        MtSingleDbPluginSupport.doWithDynamicMethods.delegate = delegate
+        MtSingleDbPluginSupport.doWithDynamicMethods ctx, application
     }
 
     def doWithWebDescriptor = MtSingleDbPluginSupport.doWithWebDescriptor
